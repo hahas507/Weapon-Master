84bc5f1c256100579c19eb27ea0fd115f6122ab6
dlgustn1245 <dlgustn1245@naver.com>
2022-01-12 23:52:11 +0900

    Add enemy's spawn patterns.
    Circle, X shape, square

--- Weapon-Master/Assets/Scripts/PlayerController.cs
+++ Weapon-Master/Assets/Scripts/PlayerController.cs
@@ -1,43 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
 
+    float moveDirX;
+    float moveDirZ;
+
+    SpawnManager spm;
     Rigidbody rb;
     CapsuleCollider capsuleCollider;
 
-    void Awake() {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody>();
         capsuleCollider = GetComponent<CapsuleCollider>();
+        spm = GameObject.Find("SpawnMgr").GetComponent<SpawnManager>();
     }
-    
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        moveDirX = Input.GetAxisRaw("Horizontal");
+        moveDirZ = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Escape)) UnityEditor.EditorApplication.isPlaying = false;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         Move();
     }
 
     void Move()
     {
-        float moveDirX = Input.GetAxisRaw("Horizontal");
-        float moveDirZ = Input.GetAxisRaw("Vertical");
-
         Vector3 moveHorizontal = transform.right * moveDirX;
         Vector3 moveVertical = transform.forward * moveDirZ;
-
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * moveSpeed;
 
         rb.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SpawnTrigger"))
+        {
+
+            spm.SpawnEnemy();
+        }
     }
 }
