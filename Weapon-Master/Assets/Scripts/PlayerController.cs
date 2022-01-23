48cf45945e06bf8d8b794878c351470a303cbaa5
dlgustn1245 <dlgustn1245@naver.com>
2022-01-23 23:53:41 +0900

    Add tageting system.
    player rotate toward monster.

--- Weapon-Master/Assets/Scripts/PlayerController.cs
+++ Weapon-Master/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float sightRange;
+    public float rotateSpeed;
+    public Camera theCamera;
+    public int maxHP;
 
     float moveDirX;
     float moveDirZ;
+    int currHP;
+    int targetIdx;
 
+    GameObject[] monsters;
+    GameObject targetMonster;
     SpawnManager spm;
     Rigidbody rb;
     CapsuleCollider capsuleCollider;
+    List<GameObject> activeMonsters = new List<GameObject>();
 
     void Awake()
     {
@@ -23,23 +31,30 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-
+        currHP = maxHP;
     }
 
     void Update()
     {
-        moveDirX = Input.GetAxisRaw("Horizontal");
-        moveDirZ = Input.GetAxisRaw("Vertical");
+        GetAxis();
+        if (Input.GetKeyDown(KeyCode.Escape)) UnityEditor.EditorApplication.isPlaying = false; //quit game
 
-        if (Input.GetKeyDown(KeyCode.Escape)) UnityEditor.EditorApplication.isPlaying = false;
+        monsters = GameObject.FindGameObjectsWithTag("Enemy"); //all monsters on field
+        SwitchTarget(GetInSightMonsters(monsters));
     }
 
     void FixedUpdate()
     {
-        Move();
+        PlayerMove();
     }
 
-    void Move()
+    void GetAxis()
+    {
+        moveDirX = Input.GetAxisRaw("Horizontal");
+        moveDirZ = Input.GetAxisRaw("Vertical");
+    }
+
+    void PlayerMove()
     {
         Vector3 moveHorizontal = transform.right * moveDirX;
         Vector3 moveVertical = transform.forward * moveDirZ;
@@ -54,6 +69,76 @@ public class PlayerController : MonoBehaviour
         {
 
             spm.SpawnEnemy();
+        }
+    }
+
+    bool IsTargetVisible(GameObject target) //determine if object is in camera sight
+    {
+        if (!IsTargetInRange(target)) return false; //if object is out of range
+
+        var planes = GeometryUtility.CalculateFrustumPlanes(theCamera);
+        var point = target.transform.position;
+
+        foreach (var plane in planes)
+        {
+            if (plane.GetDistanceToPoint(point) < 0) return false;
+        }
+        return true;
+    }
+
+    bool IsTargetInRange(GameObject target) //determine if object is in range
+    {
+        float dist = Vector3.Distance(target.transform.position, this.transform.position);
+        if (dist < sightRange) return true;
+        return false;
+    }
+
+    List<GameObject> GetInSightMonsters(GameObject[] monsters) //get objects that is both in camera sight and range
+    {
+        activeMonsters.Clear();
+        for (int i = 0; i < monsters.Length; i++)
+        {
+            if (IsTargetVisible(monsters[i])) activeMonsters.Add(monsters[i]);
+        }
+
+        SortTarget(activeMonsters);
+        return activeMonsters;
+    }
+
+    void SortTarget(List<GameObject> monstersLst)
+    {
+        monstersLst.Sort(delegate (GameObject t1, GameObject t2)
+        {
+            if (t1.transform.position.x.Equals(t2.transform.position.x))
+            {
+                return Vector3.Distance(t1.transform.position, this.transform.position).CompareTo(Vector3.Distance(t2.transform.position, this.transform.position));
+            }
+            return (t1.transform.position.x).CompareTo(t2.transform.position.x);
+        });
+    }
+
+    void SwitchTarget(List<GameObject> targets)
+    {
+        if(targets.Count == 0) return;
+
+        targetIdx = targets.IndexOf(targetMonster); //get index of target
+        if (Input.GetKeyDown(KeyCode.Q)) //shift left
+        {
+            if (!targets.Contains(targetMonster)) targetMonster = targets[0];
+            else targetIdx = targets.IndexOf(targetMonster) - 1; //previous monster
+        }
+        if (Input.GetKeyDown(KeyCode.E)) //shift right
+        {
+            if (!targets.Contains(targetMonster)) targetMonster = targets[0];
+            else targetIdx = targets.IndexOf(targetMonster) + 1; //next monster
+        }
+        
+        if (targetMonster) //not null
+        {
+           targetIdx = Mathf.Clamp(targetIdx, 0, targets.Count-1);
+            targetMonster = targets[targetIdx]; 
+            Vector3 dir = targetMonster.transform.position - this.transform.position;
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(dir), rotateSpeed * Time.deltaTime);   
         }
     }
 }
