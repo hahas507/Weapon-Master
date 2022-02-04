b2e56ca1db27fd2f267815a70d840833bc9109e1
hahas507 <aq12361@gmail.com>
2022-02-04 09:51:59 +0900

    test
    test

--- Weapon-Master/Assets/Scripts/Editor/NembeeEditor.cs
+++ Weapon-Master/Assets/Scripts/Editor/NembeeEditor.cs
@@ -10,8 +10,6 @@ public class NembeeEditor : Editor
     {
         Nembee nembee = (Nembee)target;
         Handles.color = Color.red;
-        //Handles.DrawWireArc(nembee.transform.position, Vector3.up, Vector3.forward, 360, nembee.range);
-        //Handles.color = Color.green;
-        Handles.DrawWireArc(nembee.transform.position, Vector3.up, Vector3.forward, 360, nembee.followStopRange);
+        Handles.DrawWireArc(nembee.transform.position, Vector3.up, Vector3.forward, 360, nembee.range);
     }
 }
--- Weapon-Master/Assets/Scripts/Enemy/Boss/Nembee.cs
+++ Weapon-Master/Assets/Scripts/Enemy/Boss/Nembee.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Nembee : MonoBehaviour
 {
     private float t = 0;
-    [SerializeField] [Range(0, 20)] private float speed, walkSpeed;
-    [SerializeField] [Range(0, 5)] private float atkPrepareTime;
+    [SerializeField] [Range(0, 20)] private float speed;
+    private RaycastHit raycastHit;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject fruit;
     [SerializeField] private GameObject[] Jakos;
@@ -16,25 +16,17 @@ public class Nembee : MonoBehaviour
     private Tree tree;
     private Vector3 playerPos;
     private Vector3 playerDir;
-    private Vector3 atkDir;
     private Vector3 lookAngle;
     [SerializeField] private LayerMask obsticleMask;
     [SerializeField] private LayerMask playerMask;
-
-    //[SerializeField] [Range(0, 20)] public float range;
-    [SerializeField] [Range(0, 20)] public float followStopRange, obsDetectRange;
-
+    [SerializeField] [Range(0, 20)] public float range;
     [SerializeField] [Range(0, 20)] private int jumpHeight;
     [SerializeField] [Range(0, 10)] private float jumpSpeed;
-    [SerializeField] private int currentAction;
-    private bool alreadyInAction = false;
-    [SerializeField] [Range(1, 3)] private int attackCount;
-    [SerializeField] [Range(1, 5)] private float attackDelay;
-    [SerializeField] [Range(1, 2)] private float attackForce;
-    [SerializeField] [Range(0, 4)] private float recoverTime;
-
-    private bool isIdle = false;
-
+    [SerializeField] [Range(0, 2)] private int currentAction;
+    [SerializeField] [Range(0, 10)] private int journeyTime;
+    [SerializeField] [Range(0, 360)] private float prepareAngle;
+    [SerializeField] [Range(0, 1)] private float prepareSpeed;
+    private Vector3 preparePos;
     private Rigidbody rig;
 
     private void Start()
@@ -44,218 +36,55 @@ public class Nembee : MonoBehaviour
         //StartCoroutine(HopOn("Rock"));
         //StartCoroutine(ThrowFruit());
         //StartCoroutine(CallJakos());
-        currentAction = 3;
     }
 
     private void Update()
     {
-        Actions(currentAction);
+    }
+
+    private void FixedUpdate()
+    {
     }
 
     public enum CURRENTACTION
     {
-        IDLE = 1,
         STOP,
         FOLLOW,
         HOPON,
         THROW,
-        MAKEDICISION,
-        ATTACK_1,
-        ATTACK_2,
-        REPOSITION,
     }
 
     public CURRENTACTION CAction;
 
-    private void Actions(float decisionNum)
+    private void Actions()
     {
-        CAction = (CURRENTACTION)decisionNum;
+        CAction = (CURRENTACTION)currentAction;
         switch (CAction)
         {
-            case CURRENTACTION.IDLE:
-                StartCoroutine(Idle());
-                break;
-
-            case CURRENTACTION.STOP:
-                Stop();
-                break;
-
             case CURRENTACTION.FOLLOW:
                 Follow();
                 break;
 
-            case CURRENTACTION.MAKEDICISION:
-                MakeDecision();
-                break;
-
-            case CURRENTACTION.ATTACK_1:
-                Attack_1();
-                break;
-
-            case CURRENTACTION.ATTACK_2:
-                Attack_2();
-                break;
-
-            case CURRENTACTION.REPOSITION:
-                RePosition();
+            case CURRENTACTION.STOP:
+                // Do nothing.
                 break;
 
             default:
-                rig.velocity = Vector3.zero * 0;
                 break;
         }
     }
 
-    private IEnumerator Idle()
+    private void MakeDecision()
     {
-        Stop();
-        if (!isIdle)
-        {
-            isIdle = true;
-            recoverTime = 4;
-            while (recoverTime >= 0)
-            {
-                recoverTime -= Time.deltaTime;
-                yield return null;
-            }
-            isIdle = false;
-            currentAction = 9;
-        }
-    }
-
-    private void MakeDecision() // action 6
-    {
-        rig.velocity = Vector3.zero;
         GetPlayerInfo();
-        float distance = (transform.position - playerPos).magnitude;
-        if (distance < followStopRange * 1.5f)
+        if (!Physics.Raycast(transform.position + Vector3.up, playerDir, out raycastHit, range, obsticleMask))
         {
-            currentAction = Random.Range(7, 9);
+            //currentAction = 1;
         }
         else
         {
-            currentAction = 3;
+            //그냥 navmesh쓰자
         }
-    }
-
-    #region ATTACK
-
-    private void Attack_1()
-    {
-        if (!alreadyInAction)
-        {
-            alreadyInAction = true;
-            StartCoroutine(SlashAttack());
-        }
-    }
-
-    private void Attack_2()
-    {
-        if (!alreadyInAction)
-        {
-            StartCoroutine(JumpTo());
-        }
-    }
-
-    private IEnumerator SlashAttack()
-    {
-        int counter;
-        counter = attackCount;
-        while (counter > 0)
-        {
-            GetPlayerInfo();
-            Vector3 launchDir;
-            rig.velocity = Vector3.zero;
-            transform.eulerAngles = lookAngle;
-            launchDir = atkDir;
-            Debug.Log(launchDir.magnitude);
-            if (launchDir.magnitude <= 1)
-            {
-                launchDir = transform.forward * 5;
-            }
-            rig.AddForce(launchDir * attackForce, ForceMode.VelocityChange);
-            counter--;
-            yield return new WaitForSeconds(attackDelay);
-        }
-        alreadyInAction = false;
-        currentAction = 1;
-    }
-
-    private IEnumerator JumpTo()
-    {
-        alreadyInAction = true;
-        yield return null;
-        GetPlayerInfo();
-        Vector3 startPoint = transform.position;
-        Vector3 landPoint = playerPos;
-        Vector3 passPoint = (((landPoint - startPoint) * .5f) + Vector3.up * jumpHeight) + startPoint;
-        while (t <= 1)
-        {
-            t += Time.deltaTime * jumpSpeed;
-            //아래는 애니메이션
-            //if (t > 0 && t! < .5f)
-            //{
-            //    anim.SetBool("Jump", true);
-            //}
-            //else if (t >= .5f && t < 1)
-            //{
-            //    anim.SetBool("Jump", false);
-            //    anim.SetBool("Landing", true);
-            //}
-
-            transform.eulerAngles = Vector3.up * (GetDegree(startPoint, landPoint));
-            transform.position = new Vector3(
-           ThreePointBezier(startPoint.x, passPoint.x, landPoint.x),
-           ThreePointBezier(startPoint.y, passPoint.y, landPoint.y),
-           ThreePointBezier(startPoint.z, passPoint.z, landPoint.z));
-
-            if (t >= 1)
-            {
-                //anim.SetBool("Landing", false);
-                //anim.SetTrigger("LandingAttack");
-                //yield return new WaitForSeconds(attackDelay);
-
-                rig.velocity = Vector3.zero;
-                transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-
-                //anim.SetTrigger("AfterAttack");
-                //yield return new WaitForSeconds(ClipDuration("AfterAttack"));
-
-                // isJumping = false;
-            }
-
-            yield return null;
-        }
-        if (t >= 1) t = 0;
-        currentAction = 1;
-        alreadyInAction = false;
-    }
-
-    private void RePosition()
-    {
-        GetPlayerInfo();
-        float distance = Vector3.Distance(transform.position, playerPos);
-
-        if (distance < followStopRange)
-        {
-            transform.eulerAngles = -lookAngle;
-            rig.AddForce(-transform.forward * speed * 1.5f, ForceMode.VelocityChange);
-            if (rig.velocity.magnitude > speed)
-            {
-                rig.velocity = Vector3.ClampMagnitude(rig.velocity, speed);
-            }
-        }
-        else
-        {
-            currentAction = 3;
-        }
-    }
-
-    #endregion ATTACK
-
-    private void Stop()
-    {
-        rig.velocity = Vector3.zero * 0;
     }
 
     private void Follow()
@@ -268,69 +97,11 @@ public class Nembee : MonoBehaviour
         {
             rig.velocity = Vector3.ClampMagnitude(rig.velocity, speed);
         }
-        if (Physics.Raycast(transform.position, playerDir, followStopRange, playerMask))
-        {
-            rig.velocity = Vector3.zero;
-            currentAction = 0;
-            StartCoroutine(PrepareAttack());
-        }
     }
 
-    private IEnumerator PrepareAttack()
+    private void PrepareAttack()
     {
         GetPlayerInfo();
-        int ranDirDicision = Random.Range(-1, 2);
-        float walkTime = Random.Range(1, atkPrepareTime);
-        Vector3 walkDir = lookAngle + (Vector3.up * (75 * ranDirDicision));
-
-        if (ranDirDicision != 0)
-        {
-            if (!Physics.Raycast(transform.position, walkDir, obsDetectRange, obsticleMask))
-            {
-                float timer = walkTime;
-                while (timer > 0)
-                {
-                    WalkToSide(ranDirDicision);
-                    timer -= Time.deltaTime;
-                    yield return null;
-                }
-                currentAction = 6;
-            }
-            else
-            {
-                float timer = walkTime;
-                while (timer > 0)
-                {
-                    WalkToSide(ranDirDicision);
-                    timer -= Time.deltaTime;
-                    yield return null;
-                }
-                currentAction = 6;
-            }
-        }
-        else
-        {
-            rig.velocity = Vector3.zero;
-            GetPlayerInfo();
-            transform.eulerAngles = lookAngle;
-            yield return new WaitForSeconds(1.5f);
-
-            currentAction = 6;
-        }
-    }
-
-    private void WalkToSide(int dir)
-    {
-        GetPlayerInfo();
-        transform.eulerAngles = lookAngle;
-        Vector3 direction = transform.forward;
-        var quaternion = Quaternion.Euler(dir * Vector3.up * 75);
-        Vector3 walkDir = quaternion * direction;
-        rig.AddForce(walkDir * walkSpeed, ForceMode.VelocityChange);
-        if (rig.velocity.magnitude > walkSpeed)
-        {
-            rig.velocity = Vector3.ClampMagnitude(rig.velocity, walkSpeed);
-        }
     }
 
     private IEnumerator HopOn(string _obsticle)
@@ -396,8 +167,6 @@ public class Nembee : MonoBehaviour
         }
     }
 
-    #region math
-
     private float GetDegree(Vector3 _from, Vector3 _to)
     {
         return Mathf.Atan2(_from.x - _to.x, _from.z - _to.z) * Mathf.Rad2Deg;
@@ -420,9 +189,6 @@ public class Nembee : MonoBehaviour
     {
         playerPos = player.transform.position;
         playerDir = (playerPos - transform.position).normalized;
-        atkDir = playerPos - transform.position;
         lookAngle = Vector3.up * GetDegree(transform.position, playerPos);
     }
-
-    #endregion math
 }
