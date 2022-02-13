bfb97ecfb7aba3c377ff66113550f6fd2cc1b1b7
hahas507 <aq12361@gmail.com>
2022-02-13 20:36:38 +0900

    Conflict solve

--- Weapon-Master/Assets/Scripts/TestScripts/PlayerController.cs
+++ Weapon-Master/Assets/Scripts/TestScripts/PlayerController.cs
@@ -1,57 +1,201 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    private Rigidbody rig;
+    public float moveSpeed;
+    public float sightRange;
+    public float rotateSpeed;
+    public float dodgeTimer;
+    public Camera theCamera;
+    public Text noEnemyText;
+    public int maxHP;
 
-    private bool isMoving = false;
-    private Vector3 playerPos;
+    private float moveDirX;
+    private float moveDirZ;
+    private bool isDodge;
+    private bool initRotate;
+    private bool completeInitRotate = true;
+    private int currHP;
+    private int targetIdx;
 
-    public Vector3 PLAYERPOS
+    private Vector3 velocity;
+    private GameObject[] monsters;
+    private GameObject targetMonster;
+    private SpawnManager spm;
+    private Rigidbody rb;
+    private Animator anim;
+    private BoxCollider boxCollider;
+    private List<GameObject> activeMonsters = new List<GameObject>();
+
+    private void Awake()
     {
-        get { return playerPos; }
-        private set { playerPos = value; }
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
+        spm = GameObject.Find("SpawnMgr").GetComponent<SpawnManager>();
     }
 
     private void Start()
     {
-        rig = GetComponent<Rigidbody>();
+        currHP = maxHP;
     }
 
     private void Update()
     {
-        playerPos = transform.position;
-        Move();
+        PlayerMove();
+        if (Input.GetKeyDown(KeyCode.Escape)) UnityEditor.EditorApplication.isPlaying = false; //quit game
+        if (Input.GetKeyDown(KeyCode.Space)) Dodge();
+
+        monsters = GameObject.FindGameObjectsWithTag("Enemy"); //all monsters on field
+        SwitchTarget(GetInSightMonsters(monsters));
     }
 
-    private void Move()
+    private void PlayerMove()
     {
-        float moveDirX = Input.GetAxisRaw("Horizontal");
-        float moveDirZ = Input.GetAxisRaw("Vertical");
+        moveDirX = Input.GetAxisRaw("Horizontal");
+        moveDirZ = Input.GetAxisRaw("Vertical");
+        this.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * moveDirZ);
+        this.transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * moveDirX);
+    }
 
-        Vector3 movement = new Vector3(moveDirX, 0, moveDirZ);
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SpawnTrigger"))
+        {
+            spm.SpawnEnemy();
+        }
+    }
 
-        if (movement == Vector3.zero)
+    private bool IsTargetVisible(GameObject target) //determine if object is in camera sight
+    {
+        if (!IsTargetInRange(target)) return false; //if object is out of range
+
+        var planes = GeometryUtility.CalculateFrustumPlanes(theCamera);
+        var point = target.transform.position;
+
+        foreach (var plane in planes)
         {
-            isMoving = false;
+            if (plane.GetDistanceToPoint(point) < 0) return false;
         }
-        else
+        return true;
+    }
+
+    private bool IsTargetInRange(GameObject target) //determine if object is in range
+    {
+        float dist = Vector3.Distance(target.transform.position, this.transform.position);
+        if (dist < sightRange) return true;
+        return false;
+    }
+
+    private List<GameObject> GetInSightMonsters(GameObject[] monsters) //get objects that is both in camera sight and range
+    {
+        activeMonsters.Clear();
+        for (int i = 0; i < monsters.Length; i++)
         {
-            isMoving = true;
+            if (IsTargetVisible(monsters[i])) activeMonsters.Add(monsters[i]);
         }
 
-        if (isMoving)
+        SortTarget(activeMonsters);
+        return activeMonsters;
+    }
+
+    private void SortTarget(List<GameObject> monstersLst)
+    {
+        monstersLst.Sort(delegate (GameObject t1, GameObject t2)
         {
-            transform.rotation = Quaternion.LookRotation(movement);
+            if (t1.transform.position.x.Equals(t2.transform.position.x))
+            {
+                return Vector3.Distance(t1.transform.position, this.transform.position).CompareTo(Vector3.Distance(t2.transform.position, this.transform.position));
+            }
+            return (t1.transform.position.x).CompareTo(t2.transform.position.x);
+        });
+    }
+
+    private void SwitchTarget(List<GameObject> targets)
+    {
+        if (!completeInitRotate) return;
+
+        targetIdx = GetInSightMonsters(monsters).IndexOf(targetMonster); //get index of target
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E))
+        {
+            if (targets.Count == 0)
+            {
+                noEnemyText.gameObject.SetActive(true);
+                StartCoroutine(TextFadeOut());
+                return;
+            }
+
+            if (!targets.Contains(targetMonster) || initRotate)
+            {
+                targetMonster = targets[0];
+                targetIdx = 0;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Q)) targetIdx = targets.IndexOf(targetMonster) - 1; //previous monster
+                if (Input.GetKeyDown(KeyCode.E)) targetIdx = targets.IndexOf(targetMonster) + 1; //next monster
+            }
+            initRotate = false;
         }
 
-        rig.AddForce(movement.normalized * moveSpeed, ForceMode.VelocityChange);
-        if (rig.velocity.magnitude > moveSpeed)
+        if (Input.GetKeyDown(KeyCode.Z)) //quit targeting mode, init rotation of player
         {
-            rig.velocity = Vector3.ClampMagnitude(rig.velocity, moveSpeed);
+            initRotate = true;
+            completeInitRotate = false;
+            StartCoroutine(RotateTo(Quaternion.identity));
         }
+
+        if (targetMonster && !initRotate) //not null
+        {
+            targetIdx = Mathf.Clamp(targetIdx, 0, targets.Count - 1);
+            targetMonster = targets[targetIdx];
+            Vector3 dir = targetMonster.transform.position - this.transform.position;
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(dir), rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator RotateTo(Quaternion rot)
+    {
+        while (this.transform.rotation != rot)
+        {
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, rot, 1.5f * rotateSpeed * Time.deltaTime);
+            yield return null;
+        }
+        completeInitRotate = true;
+    }
+
+    private IEnumerator TextFadeOut()
+    {
+        noEnemyText.color = new Color(noEnemyText.color.r, noEnemyText.color.g, noEnemyText.color.b, 1);
+        while (noEnemyText.color.a > 0.0f)
+        {
+            noEnemyText.color = new Color(noEnemyText.color.r, noEnemyText.color.g, noEnemyText.color.b, noEnemyText.color.a - (Time.deltaTime / 2.0f));
+            if (noEnemyText.color.a <= 0.0f) noEnemyText.gameObject.SetActive(false);
+            yield return null;
+        }
+    }
+
+    private void Dodge()
+    {
+        if (!isDodge && velocity != Vector3.zero)
+        {
+            anim.SetTrigger("Dodge");
+            StartCoroutine(DodgeDelay());
+        }
+    }
+
+    private IEnumerator DodgeDelay()
+    {
+        moveSpeed *= 2;
+        isDodge = true;
+        boxCollider.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        moveSpeed *= 0.5f;
+        boxCollider.enabled = true;
+        yield return new WaitForSeconds(dodgeTimer);
+        isDodge = false;
     }
 }
